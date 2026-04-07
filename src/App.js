1b4a990725ebff6c63afd6288dad731474f42c56
Rania Sondhi <raniasondhi@Ranias-MacBook-Air.local>
2026-04-07 23:28:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Timetable from "./pages/Timetable";
+import StudyTracker from "./pages/StudyTracker";
+import PlacementPrep from "./pages/PlacementPrep";
+import Tasks from "./pages/Tasks";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home name="Rania" />} />
+        <Route path="/timetable" element={<Timetable />} />
+        <Route path="/study" element={<StudyTracker />} />
+        <Route path="/placement" element={<PlacementPrep />} />
+        <Route path="/tasks" element={<Tasks />} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
